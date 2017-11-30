ee20da114234262c8740b8146c45f1be1a03d50e
Brett Sanders <brett.sanders@vevo.com>
2017-11-30 11:38:10 -0800

    simple values

--- GuidedTour.playground/Pages/Simple Values.xcplaygroundpage/Contents.swift
+++ GuidedTour.playground/Pages/Simple Values.xcplaygroundpage/Contents.swift
@@ -23,6 +23,7 @@ let myConstant = 42
 let implicitInteger = 70
 let implicitDouble = 70.0
 let explicitDouble: Double = 70
+let explicitFloat: Float = 4
 
 //: - Experiment:
 //: Create a constant with an explicit type of `Float` and a value of `4`.
@@ -42,6 +43,10 @@ let apples = 3
 let oranges = 5
 let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let name = "Sam"
+let number: Float = 4.0
+let greeting = "Hello \(name), my favorite number is \(number)"
 
 //: - Experiment:
 //: Use `\()` to include a floating-point calculation in a string and to include someone’s name in a greeting.
